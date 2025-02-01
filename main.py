4bdcb3a2e727080eac7500aa0387ee85a34f9aa0
DinVin24 <emisoltuzu@gmail.com>
2025-02-01 03:18:44 +0200

    Acum merge, dar trb sa "hardcodezi" bitii, versiunea si ECL

--- main.py
+++ main.py
@@ -1,82 +1,86 @@
-#https://www.thonky.com/qr-code-tutorial/
-#https://www.nayuki.io/page/creating-a-qr-code-step-by-step
-#https://youtu.be/Dap1cnMRjeA?si=IVQT9y2_Vy_b9mxW
+# https://www.thonky.com/qr-code-tutorial/
+# https://www.nayuki.io/page/creating-a-qr-code-step-by-step
+# https://youtu.be/Dap1cnMRjeA?si=IVQT9y2_Vy_b9mxW
 
 from PIL import Image
 import copy
 import numpy as np
-def timing_patterns(matrice,marime):
-    for i in range(8,marime-8):
-        if i%2==0:
-            matrice[i][6]=matrice[6][i]=1
+
+def timing_patterns(matrice, marime):
+    for i in range(8, marime - 8):
+        if i % 2 == 0:
+            matrice[i][6] = matrice[6][i] = 1
         else:
-            matrice[i][6]=matrice[6][i]=0
-def draw_finders(matrice,marime):
-    def squares(y,x):
-        for i in range(y,y+7):      #In for-urile astea desenez patratele negre
-            for j in range(x,x+7):
-                if (x+1<=j<=x+5) and (i == y+1 or i==y+5):
-                    matrice[i][j]=0
-                elif (y+1<=i<=y+5) and (j==x+1 or j==x+5):
-                    matrice[i][j]=0
+            matrice[i][6] = matrice[6][i] = 0
+
+def draw_finders(matrice, marime):
+    def squares(y, x):
+        for i in range(y, y + 7):  # In for-urile astea desenez patratele negre
+            for j in range(x, x + 7):
+                if (x + 1 <= j <= x + 5) and (i == y + 1 or i == y + 5):
+                    matrice[i][j] = 0
+                elif (y + 1 <= i <= y + 5) and (j == x + 1 or j == x + 5):
+                    matrice[i][j] = 0
                 else:
-                    matrice[i][j]=1
-        #de aici desenez colturile albe
-        if y==x==0:
+                    matrice[i][j] = 1
+        # de aici desenez colturile albe
+        if y == x == 0:
             for i in range(8):
-                matrice[y+i][x+7]=matrice[y+7][x+i]=0
-        elif y==0 and x==marime-7:
-            x-=1
+                matrice[y + i][x + 7] = matrice[y + 7][x + i] = 0
+        elif y == 0 and x == marime - 7:
+            x -= 1
             for i in range(8):
-                matrice[y+i][x]=matrice[y+7][x+i]=0
-        elif y==marime-7 and x==0:
-            y-=1
+                matrice[y + i][x] = matrice[y + 7][x + i] = 0
+        elif y == marime - 7 and x == 0:
+            y -= 1
             for i in range(8):
-                matrice[y][x+i]=matrice[y+i][x+7]=0
+                matrice[y][x + i] = matrice[y + i][x + 7] = 0
 
-    squares(0,0),squares(0,marime-7),squares(marime-7,0)
+    squares(0, 0), squares(0, marime - 7), squares(marime - 7, 0)
 
-def draw_dummy_format_bits(matrice,marime):
-#Astea-s placeholders pt bitii despre error correction, cred...
-    #mai intai abordam coltul din stanga sus
+def draw_dummy_format_bits(matrice, marime):
+    # Astea-s placeholders pt bitii despre error correction, cred...
+    # mai intai abordam coltul din stanga sus
     for i in range(9):
-        if i!=6:
-            matrice[8][i]=matrice[i][8]=0
-    #ACUM coltul din dreapta
+        if i != 6:
+            matrice[8][i] = matrice[i][8] = 0
+    # ACUM coltul din dreapta
     for i in range(8):
-        matrice[8][marime-8+i]=0
-    #si coltu din stanga jos
+        matrice[8][marime - 8 + i] = 0
+    # si coltu din stanga jos
     for i in range(8):
-        matrice[marime-8+i][8]=0
-        if i==0:
-            matrice[marime-8+i][8]=1
+        matrice[marime - 8 + i][8] = 0
+        if i == 0:
+            matrice[marime - 8 + i][8] = 1
 
-def am_loc(matrice,y,x):
-#in functia asta verific daca sunt pe o pozitie valida, cand imi pun alignment patterns (patratelele mici)
-    if matrice[y][x] !=8:
+def am_loc(matrice, y, x):
+    # in functia asta verific daca sunt pe o pozitie valida, cand imi pun alignment patterns (patratelele mici)
+    if matrice[y][x] != 8:
         return False
-    if matrice[y-2][x-2]!=8 or matrice[y-2][x+2]!=8 or matrice[y+2][x-2]!=8 or matrice[y+2][x+2]!=8:
+    if matrice[y - 2][x - 2] != 8 or matrice[y - 2][x + 2] != 8 or matrice[y + 2][x - 2] != 8 or matrice[y + 2][
+        x + 2] != 8:
         return False
     return True
+
 def calculate_alignment_coords(versiune):
-#aici incercam sa caluclez coordonatele pt fiecare versiune pana mi-am dat seama ca dureaza mult si n-are rost
-#acum folosim o functie care are un dictionar, deci este mult mai rapid...
-    L=[]
-    if versiune==1:
+    # aici incercam sa caluclez coordonatele pt fiecare versiune pana mi-am dat seama ca dureaza mult si n-are rost
+    # acum folosim o functie care are un dictionar, deci este mult mai rapid...
+    L = []
+    if versiune == 1:
         return L
     L.append(6)
-    L.append(10+4*versiune)
+    L.append(10 + 4 * versiune)
     if versiune < 7:
         return L
     if versiune < 14:
-        L.append(8+2*versiune)
+        L.append(8 + 2 * versiune)
     elif versiune < 21:
         if versiune < 17:
             L.append(26)
-            L.append(18+2*versiune)
+            L.append(18 + 2 * versiune)
         elif versiune < 20:
             L.append(30)
-            L.append(30+2*versiune)
+            L.append(30 + 2 * versiune)
         else:
             L.append(34)
             L.append(62)
@@ -127,65 +131,65 @@ def get_alignment_pattern_positions(v):
     }
     return alignment_positions.get(v, [])
 
-def draw_alignment_patterns(matrice,versiune):
-#functia asta e destul de lenesa...
+def draw_alignment_patterns(matrice, versiune):
+    # functia asta e destul de lenesa...
     L = get_alignment_pattern_positions(versiune)
     # print(L)    #DEBUGGING
     for i in L:
         for j in L:
-            if am_loc(matrice,i,j):
-                i-=2
-                j-=2
-                for x in range(5):      #desenex patrat 5x5 negru
+            if am_loc(matrice, i, j):
+                i -= 2
+                j -= 2
+                for x in range(5):  # desenex patrat 5x5 negru
                     for y in range(5):
-                        matrice[i+y][j+x]=1
-                i+=1
-                j+=1
-                for x in range(3):      #desenex patrat 3x3 alb
+                        matrice[i + y][j + x] = 1
+                i += 1
+                j += 1
+                for x in range(3):  # desenex patrat 3x3 alb
                     for y in range(3):
-                        matrice[i+y][j+x]=0
-                j+=1
-                i+=1
-                matrice[i][j]=1     #desenex un pixel negru
+                        matrice[i + y][j + x] = 0
+                j += 1
+                i += 1
+                matrice[i][j] = 1  # desenex un pixel negru
 
 def calculate_version_information(v):
-    if v<7:
+    if v < 7:
         return None
     vbits = v << 12
 
-    #Asta-i un polinom in Z2, abia acum am aflat de el
+    # Asta-i un polinom in Z2, abia acum am aflat de el
     # x^12 + x^11 + x^10 + x^9 + x^8 + x^5 + x^2 + 1
     generator = 0b1111100100101
-    #Acum facem o impartire polinomiala pls dont ask how it works! :D
-    for i in range(6,-1,-1):
-        if vbits & (1 << (i+12)):
-            vbits ^= generator<<i
-    #deci dupa ce impartim vbits la generator, ne ramane restul in vbits
-    #vbits asta il concatenam la v-ul nostru initial
-    #practic in binar o sa avem primii 6 biti pentru numarul informatiei
-    #si ultimii 12 biti sunt restul impartirii la generator
-    #URASC URASC URASC URASC URASC URASC URASC URASC URASC URASC URASC URASC
+    # Acum facem o impartire polinomiala pls dont ask how it works! :D
+    for i in range(6, -1, -1):
+        if vbits & (1 << (i + 12)):
+            vbits ^= generator << i
+    # deci dupa ce impartim vbits la generator, ne ramane restul in vbits
+    # vbits asta il concatenam la v-ul nostru initial
+    # practic in binar o sa avem primii 6 biti pentru numarul informatiei
+    # si ultimii 12 biti sunt restul impartirii la generator
+    # URASC URASC URASC URASC URASC URASC URASC URASC URASC URASC URASC URASC
 
-    vinfo = (v<<12) | vbits
+    vinfo = (v << 12) | vbits
     return f"{vinfo:018b}"
 
-def draw_version_information(matrice,versiune,marime):
-#Cu astea se deseneaza cei 18 biti de informatie pentru versiune
+def draw_version_information(matrice, versiune, marime):
+    # Cu astea se deseneaza cei 18 biti de informatie pentru versiune
     if versiune < 7:
         return None
     aux = bitString = calculate_version_information(versiune)[::-1]
     for j in range(6):
-        for i in range(marime-11,marime-8):
+        for i in range(marime - 11, marime - 8):
             matrice[i][j] = int(bitString[0])
             bitString = bitString[1:]
     bitString = aux
     for i in range(6):
-        for j in range(marime-11,marime-8):
+        for j in range(marime - 11, marime - 8):
             matrice[i][j] = int(bitString[0])
             bitString = bitString[1:]
 
 def zigzag(matrix):
-#Cread de Alex :3
+    # Creat de Alex :3
     l = len(matrix)
     rev = 0
     correction = 0
@@ -197,30 +201,30 @@ def zigzag(matrix):
             for i in range(l - 1, -1, -1):
                 for j in range(l - k * 2 - 1 + correction, l - k * 2 - 3 + correction, -1):
                     if matrix[i][j] == 8:
-                        rez.append((i,j))
+                        rez.append((i, j))
             rev = 1
         else:
             for i in range(0, l):
                 for j in range(l - k * 2 - 1 + correction, l - k * 2 - 3 + correction, -1):
                     if matrix[i][j] == 8:
-                        rez.append((i,j))
+                        rez.append((i, j))
             rev = 0
     return rez
 
-def save_bits(matrice,msg):
+def save_bits(matrice, msg):
     L = zigzag(matrice)
-    if len(L)>len(msg):
-        x = len(L)-len(msg)
-        msg = msg + ("0"*x)
+    if len(L) > len(msg):
+        x = len(L) - len(msg)
+        msg = msg + ("0" * x)
     while L:
-        i,j = L[0]
+        i, j = L[0]
         matrice[i][j] = int(msg[0])
-        msg=msg[1:]
+        msg = msg[1:]
         L = L[1:]
 
-def apply_mask(matrix, mask_id,template):
+def apply_mask(matrix, mask_id, template,ECL):
     size = len(matrix)
-    # masked_matrix = [[0] * size for _ in range(size)]
+    masked_matrix = copy.deepcopy(matrix)
     for row in range(size):
         for col in range(size):
             apply = False
@@ -241,51 +245,189 @@ def apply_mask(matrix, mask_id,template):
             elif mask_id == 7:
                 apply = ((row + col) % 2 + (row * col) % 3) % 2 == 0
 
-            if apply and template[row][col]==8 :
-                matrix[row][col] ^= 1
-    # return masked_matrix
+            if apply and template[row][col] == 8:
+                masked_matrix[row][col] ^= 1
+    draw_format_bits(masked_matrix, ECL,mask_id)
+    return masked_matrix
 
-def find_best_mask(matrice):
+def calculate_format_string(ecl, mask):
+#returneaza un sir de biti
+#functia asta este la fel de enervanta ca calculate version information...
+    ecl_bits = {    #dictionar cu toate nivelele de error correction
+        'L': '01',
+        'M': '00',
+        'Q': '11',
+        'H': '10'
+    }
+    ecl_bit = ecl_bits[ecl]
+    mask_bit = f"{mask:03b}"  #Valoarea mastii o transformam in binar
+    combined_bits = ecl_bit + mask_bit  # concatenam astea doua si ramanem cu 5 biti
 
+    # BCH Error Correction (modulo-2 division) aici nu mai stiu ce se intampla...
+    format_info = int(combined_bits, 2) << 10
+    generator = 0b10100110111  # polinomu' generator
 
-def return_mat(x,msg):
-    if not (1<=x<=40):
+    for i in range(14, 9, -1):  #impartim cele doua polinoame
+        if format_info & (1 << i):
+            format_info ^= generator << (i - 10)
+
+    bch_code = format_info & 0b1111111111  #luam doar ultimii 10 biti
+
+    # aplicam o masca
+    format_string = (int(combined_bits, 2) << 10) | bch_code
+    format_string ^= 0b101010000010010  # si o masca XOR
+
+    return f"{format_string:015b}"
+
+def draw_format_bits(matrix,ecl,mask_id):
+    marime = len(matrix)
+    bitString = calculate_format_string(ecl, mask_id)
+    #aici memorez in coltu stanga sus
+    aux = bitString[::1]
+    for i in range(9):
+        if i == 6:
+            continue
+        matrix[8][i] = int(aux[0])
+        aux = aux[1:]
+    for i in range(8):
+        if i == 1:
+            continue
+        matrix[7-i][8] = int(aux[0])
+        aux = aux[1:]
+
+    #aici o sa memorez in coltul din dreapta
+    aux = bitString[7:]
+    for i in range(8):
+        matrix[8][marime-8+i] = int(aux[0])
+        aux = aux[1:]
+    aux = bitString[:7]
+    #aici memorez in coltul din stanga
+    for i in range(7):
+        matrix[marime-1-i][8] = int(aux[0])
+        aux = aux[1:]
+
+def transpose_matrix(matrix):
+    return [list(row) for row in zip(*matrix)]
+
+def count_horizontal_patterns(matrix):
+    size = len(matrix)
+    count = 0
+    for row in matrix:
+        streak = 1
+        for col in range(1, size):
+            if row[col] == row[col - 1]:
+                streak += 1
+            else:
+                if streak >= 5:
+                    count += streak - 2
+                streak = 1
+        if streak >= 5:
+            count += streak - 2
+
+    return count
+
+def count_vertical_patterns(matrix):
+    transposed = transpose_matrix(matrix)
+    return count_horizontal_patterns(transposed)
+
+def count_2x2_patterns(matrix):
+    size = len(matrix)
+    count = 0
+
+    for row in range(size - 1):
+        for col in range(size - 1):
+            if (matrix[row][col] == matrix[row][col + 1] ==
+                    matrix[row + 1][col] == matrix[row + 1][col + 1]):
+                count += 3
+
+    return count
+
+def find_horizontal_finder_patterns(matrix):
+#TE ROG REPARA FUNCTIA ASTA, PT CA NU SE CALCULEAZA CORECT :(((((((((((((
+    count = 0
+    for linie in matrix:
+        #asta-i pt cele din mijloc
+        for i in range(len(linie)-10):
+            if linie[i:i+11] == [0,0,0,0,1,0,1,1,1,0,1] or linie[i:i+11] == [1,0,1,1,1,0,1,0,0,0,0]:
+                count+=1
+        #asta-i pt cele din marginea stanga
+        if linie[:7]==[1,0,1,1,1,0,1]:
+            count+=1
+        #asta-i pt cele din marginea dreapta
+        if linie[len(linie)-7:] == [1,0,1,1,1,0,1]:
+            count+=1
+
+    return count*40
+
+def find_vertical_finder_patterns(matrix):
+    transposed = transpose_matrix(matrix)
+    return find_horizontal_finder_patterns(transposed)
+
+def dark_light_ratio(matrix):
+    size = len(matrix)
+    total_modules = size * size
+    dark_modules = sum(sum(row) for row in matrix)
+    ratio = int((dark_modules / total_modules) * 100)
+    if ratio < 50:
+        ratio = 50 - ratio
+    else:
+        ratio -= 50
+    if ratio <=5:
+        return 0
+    ratio = (ratio//5) + (-1 if ratio%5 == 0 else 0)
+    return ratio
+
+def find_best_mask(matrix, template,ECL):
+    rez = []
+    for i in range(8):
+        cnt = 0
+        mask = apply_mask(matrix, i, template,ECL)
+        cnt += find_horizontal_finder_patterns(mask)+find_vertical_finder_patterns(mask)
+        cnt += count_horizontal_patterns(mask)+count_vertical_patterns(mask)
+        cnt += count_2x2_patterns(mask)
+        cnt += dark_light_ratio(mask)*10
+        rez.append((i, cnt))
+    rez = sorted(rez, key=lambda x: x[1])
+    #rez[0][0] contine masca cea mai buna!!!!
+    return rez[0][0]
+
+def return_mat(x, msg,ECL):
+    if not (1 <= x <= 40):
         return None
     versiune = x
     marime = (versiune - 1) * 4 + 21
-    matrice = [[8] * marime for i in range(marime)]
-    draw_finders(matrice,marime)
-    draw_alignment_patterns(matrice,versiune)
-    draw_dummy_format_bits(matrice,marime)
-    timing_patterns(matrice,marime)
-    draw_version_information(matrice,versiune,marime)
+    matrice = [[8] * marime for _ in range(marime)]
+    draw_finders(matrice, marime)
+    draw_alignment_patterns(matrice, versiune)
+    draw_dummy_format_bits(matrice, marime)
+    timing_patterns(matrice, marime)
+    draw_version_information(matrice, versiune, marime)
     template = copy.deepcopy(matrice)
-    save_bits(matrice,msg)
-    apply_mask(matrice,5,template)
+    save_bits(matrice, msg)
+    matrice = apply_mask(matrice,find_best_mask(matrice, template,ECL) , template,ECL)
     return matrice
 
-
-ver = 4
-message = "01000011011101000001011011100110000100100000011000010111001001100101001000000111001101100001011100000111010001100101001000000110110101100101011100100110010100100000011011010110000101110010011010010010110000100000011100100110111101110011011010010110100100101100001000000110011001110010011101010110110101101111011000010111001101100101001000000111001101101001001000000111101001100101011011010110111101100001011100110110010100100001001000000011101001000100000011101100000100011110110000010001111011000001000111101100000100011110110000010001111011000001000111101100000100011110110000010001111011000001000111101100000100011110110000010001111011000100111000010111111011100111101110100010100101011101101110101010001011000110101101001111100110000001111101111100101011000101100000001110011101110000111010111011"
-poza = return_mat(ver,message)
-for linie in poza:
-    print(linie)
+ver = 1
+ECL = 'L'
+message = "0100000100010100100001100101011011000110110001101111001011000010000001110111011011110111001001101100011001000010000100100000001100010011001000110011000010000101101010010101111000000111000010100011011011001001"
+poza = return_mat(ver, message, ECL)
+# for linie in poza:
+#     print(linie)
 
 
 # DE AICI ESTE DOAR AFISAREA MATRICEI IN POZA
 color_map = {
     0: (255, 255, 255),  # White
-    1: (0, 0, 0),        # Black
-    8: (128, 128, 128)   # Gray
+    1: (0, 0, 0),  # Black
+    8: (128, 128, 128)  # Gray
 }
 image = Image.new("RGB", (len(poza), len(poza)))
 pixels = image.load()
 for i in range(len(poza)):
     for j in range(len(poza)):
-        pixels[j,i]=color_map[poza[i][j]]
+        pixels[j, i] = color_map[poza[i][j]]
 scaling_factor = 50
 new_size = (len(poza) * scaling_factor, len(poza) * scaling_factor)
 upscaled_image = image.resize(new_size, Image.NEAREST)
 upscaled_image.save("upscaled_matrix_image.png")
 upscaled_image.show()
-
