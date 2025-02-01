5e3a09e4b446ba2f4256a73bbd5a19c61ca2c5b8
DinVin24 <emisoltuzu@gmail.com>
2025-02-01 21:22:37 +0200

    Merge, dar trb sa dai import la modulele lui Teo!!!

---
+++ QRGenerator.py
@@ -0,0 +1,31 @@
+import reed_solomon as rs
+import version_check as vc
+import QRdrawer as qd
+from PIL import Image
+
+ECL = input("ERROR =")
+input_text = (input("Message: "))
+ver,size = vc.version_check(input_text,ECL)
+msg = rs.final_codewords(input_text,ECL)
+
+poza = qd.return_mat(ver,msg,ECL)
+
+for linie in poza:
+    print (linie)
+
+# DE AICI ESTE DOAR AFISAREA MATRICEI IN POZA
+color_map = {
+    0: (255, 255, 255),  # White
+    1: (0, 0, 0),  # Black
+    8: (128, 128, 128)  # Gray
+}
+image = Image.new("RGB", (len(poza), len(poza)))
+pixels = image.load()
+for i in range(len(poza)):
+    for j in range(len(poza)):
+        pixels[j, i] = color_map[poza[i][j]]
+scaling_factor = 50
+new_size = (len(poza) * scaling_factor, len(poza) * scaling_factor)
+upscaled_image = image.resize(new_size, Image.NEAREST)
+upscaled_image.save("test_de_la_teo.png")
+upscaled_image.show()
